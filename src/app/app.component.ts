f22e00ee2f5f3f5657bf20f0fd088e1c7a16bdc0
Andleeb Ghazal <andleeb@example.com>
2024-07-24 21:49:58 +0500

    Added Map View toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'location';
+export class AppComponent implements OnInit {
+  title = '2D Location Map';
+  showMap: boolean = false;
+
+  ngOnInit() {
+    if (this.showMap) {
+      this.initializeMap();
+    }
+  }
+
+  toggleMap() {
+    this.showMap = !this.showMap;
+    if (this.showMap) {
+      setTimeout(() => this.initializeMap(), 0); // Initialize map after the view updates
+    }
+  }
+
+  initializeMap() {
+   
+    const map = L.map('map').setView([51.505, -0.09], 3);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 }
